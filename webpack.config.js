c60bffc4fde433e1334971c5e5e2cb652a0ecdbc
Cliff Crerar <cliff@gmai.com>
2020-02-04 22:16:55 +0200

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'public')
     },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
